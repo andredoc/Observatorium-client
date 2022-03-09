20e90e2b197389c85feb49a7c331860c436a3d0c
Roberto Cadenas <robertocads7@gmail.com>
2022-03-09 13:32:21 +0100

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,28 @@
 import './Footer.css'
 import { useLocation } from 'react-router-dom'
 
+
 const Footer = ({ path }) => {
     const { pathname } = useLocation()
     if (pathname === "/items-list") {
+        return null
+    } else if (pathname === "/item/:item_id") {
         return null
     } else if (pathname === "/mars/carousel") {
         return null
     } else if (pathname === "/mars/weather") {
         return null
-    }
+    } else if (pathname === "/earth") {
+        return null
+    } else if (pathname === "/apod") {
+        return null
+    } else if (pathname === "/profile") {
+        return null
+    } else if (pathname === "/profile/userItems") {
+        return null
+    } else if (pathname === "/admin") {
+        return null
+    } 
     return (<footer>Created by André Documet, Ricardo Ronchetti y Roberto Cadenas</footer>)
 }
 
